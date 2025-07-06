1c09bc9f5906e4b0593bb00541b513f68cea2b9e
Vladislav Dobrovolskiy <53941414+froOzzy@users.noreply.github.com>
2025-07-06 09:03:47 +0300

    Update snacks.lua

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,10 +1,12 @@
 return {
-  {
-    "bennypowers/snacks.nvim",
-    opts = {
-      rg = {
-        args = { "--color=never", "--fixed-strings", "--smart-case" },
+  "folke/snacks.nvim",
+  opts = {
+    sources = {
+      picker = {
+        grep = {
+          regex = false,
+        },
       },
     },
   },
-}
+ }
