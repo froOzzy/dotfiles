ec2be049924d6aa0b721edb0726b49e0d78a5039
Vladislav Dobrovolskiy <53941414+froOzzy@users.noreply.github.com>
2025-07-21 19:55:24 +0300

    Update trouble.lua

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,6 +1,15 @@
 return {
   {
     "folke/trouble.nvim",
-    opts = { use_diagnostic_signs = true },
+    opts = {
+      use_diagnostic_signs = true,
+      modes = {
+        symbols = {
+          win = {
+            position = "bottom",
+          },
+        },
+      },
+    },
   },
 }
